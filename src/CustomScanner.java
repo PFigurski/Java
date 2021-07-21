23d2b209bbf30cd73f2600bda45f586798077643
PFigurski <figurski.pawel9@gmail.com>
2021-07-21 13:47:22 +0200

    sample commit message

---
+++ src/CustomScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CustomScanner {
+
+   /* public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę, podaj swoje imię");
+        String name = scanner.nextLine();
+        System.out.println("Cześć " + name + " !");
+    }*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę, podaj pierwszą liczbę");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Kwadrat tej liczby to: "+ firstNumber*firstNumber);
+    }
+}
---
+++ src/zmienne.java
@@ -0,0 +1,18 @@
+// zarezerwowane miejsce w pamięci z określonym typem danych
+public class zmienne {
+
+    public static void main(String[] args) {
+        int firstNumber,secondNumber,result;
+
+
+        firstNumber = 1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+
+
+        String name = "Kasia";
+        System.out.println(name);
+        System.out.println(result);
+    }
+
+}
