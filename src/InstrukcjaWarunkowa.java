ba46a953e18648eb6706161d3e382e29597c581f
PFigurski <figurski.pawel9@gmail.com>
2021-07-28 22:31:08 +0200

    if else

---
+++ src/InstrukcjaWarunkowa.java
@@ -0,0 +1,12 @@
+public class  InstrukcjaWarunkowa {
+
+    public static void main(String[] args) {
+        float dzielna= 4;
+        float dzielnik= 0;
+        if(dzielnik !=0){
+            System.out.println("Wynik dzielenia to: " + (dzielna/dzielnik));
+        } else {
+            System.out.println("Proszę podaj inną liczbę");
+        }
+    }
+}
