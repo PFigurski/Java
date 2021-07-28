e2bce60ea8c518a3084e5a1d53f897bbbf5b0597
PFigurski <figurski.pawel9@gmail.com>
2021-07-28 23:00:52 +0200

    poprawa na angielsie zmienne

--- src/PracaDomowaIf.java
+++ src/PracaDomowaIf.java
@@ -4,8 +4,8 @@ public class PracaDomowaIf {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj ile masz lat :");
-        int wiek = scanner.nextInt();
-        if (wiek >= 18){
+        int age = scanner.nextInt();
+        if (age >= 18){
             System.out.println("Jesteś pełnoletni, możesz wejść");
         }else {
             System.out.println("Nie masz 18 lat, nie możesz wejść");
