b21a173192183a4c3dc9a2d8f6a24f1b739ac993
PFigurski <figurski.pawel9@gmail.com>
2021-08-05 15:45:37 +0200

    while dowhile petla switch i tablice

--- src/Tablice.java
+++ src/Tablice.java
@@ -5,22 +5,22 @@ public class Tablice {
         imiona[1] = "Basia";
         imiona[2] = "Michal";
 
-       /* System.out.println(imiona[0]);
+        System.out.println(imiona[0]);
         System.out.println(imiona[1]);
-        System.out.println(imiona[2]);*/
+        System.out.println(imiona[2]);
 
         int[] lottoNumbers = new int[] {1,2,3,4,5,6};
-       /* System.out.println(lottoNumbers[0]);
+       System.out.println(lottoNumbers[0]);
         System.out.println(lottoNumbers[1]);
         System.out.println(lottoNumbers[2]);
         System.out.println(lottoNumbers[3]);
         System.out.println(lottoNumbers[4]);
         System.out.println(lottoNumbers[5]);
 
-        System.out.println(lottoNumbers.length);*/
+        System.out.println(lottoNumbers.length);
 
         for (int i=0; i<imiona.length; i++){
             System.out.println(lottoNumbers[i]);
         }
 
-}
+}}
