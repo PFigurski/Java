135b92077e946b1e6e9902fb9133f72e7b18789b
PFigurski <figurski.pawel9@gmail.com>
2021-08-05 12:32:04 +0200

    praca domowa

--- src/PracaDomowaIf.java
+++ src/PracaDomowaIf.java
@@ -5,10 +5,12 @@ public class PracaDomowaIf {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj ile masz lat :");
         int age = scanner.nextInt();
-        if (age >= 18) {
-            System.out.println("Jesteś pełnoletni, możesz wejść");
+        if (age>120) {
+            System.out.println("Chyba za dużo");
         }else if (age<1){
-            System.out.println("Podaj wartość dodatnią");
+            System.out.println("Podaj wartość właściwą");
+        }else if (age >= 18){
+            System.out.println("Jesteś pełnoletni, możesz wejść");
         }else{
             System.out.println("Nie masz 18 lat, nie możesz wejść");
         }
