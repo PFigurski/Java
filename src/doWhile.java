69b6eda2dc9aebb1fecd5be6c4000b0ba27e9487
PFigurski <figurski.pawel9@gmail.com>
2021-08-05 15:30:01 +0200

    while dowhile petla switch i tablice

---
+++ src/doWhile.java
@@ -0,0 +1,10 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int number = 5;
+        do {
+            System.out.println("Moja wartosc to: " +number + " i jestem mniejszy od 20");
+            number++;
+        }
+        while (number < 20);
+    }
+}
