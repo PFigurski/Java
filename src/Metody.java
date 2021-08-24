75a8956b65c5b555164ba62e2d325c0b16a553f0
PFigurski <figurski.pawel9@gmail.com>
2021-08-24 13:09:48 +0200

    Metody

--- src/Metody.java
+++ src/Metody.java
@@ -1,14 +1,14 @@
 public class Metody {
 
-    public void policzWynik() {
+   /* public void policzWynik() {
         System.out.println("Zaraz policze wynik");
         int result = 0;
         for (int i=0; i<100; i++) {
             result = result+i;
         }
         System.out.println("Rezultat to: " +result);
-    }
-    public int pobierzWynik() {
+    }*/
+   /* public int pobierzWynik() {
         System.out.println("Zaraz policze wynik");
         int result = 0;
         for (int i=0; i<100; i++) {
@@ -16,5 +16,23 @@ public class Metody {
         }
         System.out.println("Rezultat to: " +result);
         return result;
+    }*/
+
+    public void policzParam(int number) {
+        System.out.println("Number ma wartość: "+number);
+        System.out.println("Zaraz policze wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result + i;
+        }
+        System.out.println("Rezultat to: " + result);
+    }
+
+    public int add(int firstNumber, int secondNumber) {
+       // System.out.println("Suma to: "+(firstNumber+secondNumber));
+        if(firstNumber==0){
+            return 0;
+        }
+        return firstNumber + secondNumber;
     }
 }
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -1,8 +1,16 @@
 public class MetodyTest {
     public static void main(String[] args) {
         Metody metody = new Metody();
-        int result = metody.pobierzWynik();
-        int result2 = result*2;
-        System.out.println("Rezultat po mnożeniu to: "+ result2);
+     /*  int result = metody.pobierzWynik();
+       int result2 = result * 2;
+        System.out.println("Rezultat po mnożeniu to: " + result2);*/
+       /* metody.policzParam(2);
+        metody.policzParam(100);
+        metody.policzParam(23);*/
+
+        int result = metody.add(2,3);
+        System.out.println(metody.add(2,6));
+        System.out.println(result);
+
     }
 }
