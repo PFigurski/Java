366b421309bc9aff7696ebfcb7bd32fe900c47a0
arek.bucholc <arkadiuszbucholc@monogo.pl>
2021-11-08 16:23:25 +0100

    identifiers vol 1 11 minuta

---
+++ src/com/tester/app/App.java
@@ -0,0 +1,12 @@
+package com.tester.app;
+
+import com.tester.app.database.DatabaseConnection;
+import com.tester.app.database.DatabaseUtilis;
+
+public class App {
+    public static void main(String[] args) {
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        DatabaseUtilis databaseUtilis = new DatabaseUtilis();
+        
+    }
+}
